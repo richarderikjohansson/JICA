25d3088f8767ee2ac596cf7e6d0127606636bb88
Richard Johansson <richard.johansson@irf.se>
2026-01-13 13:54:19 +0100

    richard scripting

--- rscripts/id50.py
+++ rscripts/id50.py
@@ -1,5 +1,12 @@
 from jica.io import find_file_from_id
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 import numpy as np
 
 
-fp = find_file_from_id(id=0)
+fp = find_file_from_id(id=50)
+dataset = np.load(fp)
+print(dataset)
+
+time = dataset["time"]
+specsum = dataset["p1p2m1m2"]
