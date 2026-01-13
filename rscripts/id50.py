de61ff0deca327c3077f0a14c6cf7ed6ab8af0be
Richard Johansson <richard.johansson@irf.se>
2026-01-13 18:46:42 +0100

    added datarequest 2, and concatinated that data

--- rscripts/id50.py
+++ rscripts/id50.py
@@ -1,12 +1,1 @@
-from jica.io import find_file_from_id
-import matplotlib.pyplot as plt
-from matplotlib.gridspec import GridSpec
-import numpy as np
 
-
-fp = find_file_from_id(id=50)
-dataset = np.load(fp)
-print(dataset)
-
-time = dataset["time"]
-specsum = dataset["p1p2m1m2"]
